f407bb0f50c18411eeff3000d54bb566c8a9d9f1
shyam <shyam20998@gmail.com>
2024-11-23 14:24:46 +0530

    secure cookies in prod

--- server/utils/attachCookies.js
+++ server/utils/attachCookies.js
@@ -2,8 +2,8 @@ const attachCookies = (res, token, expires = 24 * 60 * 60 * 1000) => {
 	res.cookie("token", token, {
 		httpOnly: true,
 		expires: new Date(Date.now() + expires),
-		sameSite: "strict"
-		//secure: process.env.NODE_ENV === "production"
+		sameSite: "strict",
+		secure: process.env.NODE_ENV === "production"
 	});
 };
 
