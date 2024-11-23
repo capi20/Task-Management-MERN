400921f797274a172af243042877644ec82c2935
shyam <shyam20998@gmail.com>
2024-11-23 15:32:13 +0530

    added API logger

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ import morgan from "morgan";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
 import notFoundMiddleware from "./middleware/not-found.js";
 import connectDB from "./db/connect.js";
-
+import fs from "fs";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 import cors from "cors";
@@ -25,22 +25,32 @@ import { startScheduler } from "./scheduler/index.js";
 dotenv.config();
 const app = express();
 
-if (process.env.NODE_ENV !== "production") {
-	app.use(morgan("dev"));
-}
+// if (process.env.NODE_ENV !== "production") {
+// 	app.use(morgan("dev"));
+// }
 
+// current dir path
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// Create a write stream for the log file
+const logStream = fs.createWriteStream(path.join(__dirname, "access.log"), {
+	flags: "a" // Append to the file, do not overwrite
+});
+
+// Use morgan to log requests in 'combined' format to the file
+app.use(morgan("combined", { stream: logStream }));
+
+// API limiter 100 requests per minute
 const apiLimiter = rateLimiter({
 	windowMs: 1 * 60 * 1000,
-	max: 8,
+	max: 100,
 	message: {
 		status: 429,
 		message: "Too many requests, please try again later."
 	}
 });
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-app.use(express.static(path.resolve(__dirname, "../client/dist")));
-
+// Allow other domains
 app.use(
 	cors({
 		origin: "http://localhost:5173", // Frontend URL
@@ -49,12 +59,15 @@ app.use(
 );
 
 app.use(apiLimiter);
+
+app.use(express.static(path.resolve(__dirname, "../client/dist")));
 app.use(express.json());
 app.use(cookieParser());
 app.use(helmet());
 app.use(xss());
 app.use(mongoSanitize());
 
+// Routes
 app.use("/api/auth", authRouter);
 app.use("/api/tasks", authenticateUser, taskRouter);
 
@@ -70,6 +83,7 @@ const port = process.env.PORT || 5000;
 // start task reminder scheduler
 startScheduler();
 
+// Connect to DB and start server
 const start = async () => {
 	try {
 		await connectDB(process.env.MONGO_URL);
